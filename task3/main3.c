a7cfdc72485f17a0226c15067cfa0e49427ebb80
AndreAMolkov <molkov_aa@mail.ru>
2017-11-14 16:59:22 +0300

    the third task is done

--- task3/main3.c
+++ task3/main3.c
@@ -1,3 +1,42 @@
-#define _CRT_N0_WINDOWS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include
+#include<stdlib.h>
+#include "task3.h"
+#define SIZE 100
+int main()
+{
+	
+	int i, line = 0, weight = 0, height;
+	int weightGap,score=0;
+	char *result = { " " };
+	char buf[SIZE] = { " " };
+	char gap[] = { " " };
+	printf("Enter numeros line :");
+	scanf("%i", &height);
+	weight=height+height-1;
+	weightGap=height;
+
+		for (line=0;line<=weight-1;line=line+2)
+		{
+			weightGap --;
+				for (i=weightGap;i>0 ;i--)
+				{
+					printf("%s", gap);
+				}
+			result = layout(buf, line);
+			printf("%s", result);
+			printf("\n");
+		}
+
+		
+	
+
+	
+	
+	
+	
+	
+
+
+	return 0;
+}
--- task3/task3.c
+++ task3/task3.c
@@ -1,3 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_N0_WINDOWS
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 20
+
+char * layout(char buf[], int line)
+{
+	int i = 0;
+	for (i = 0; i<=line;i++)
+	{
+		buf[i] = '*';
+	}
+
+
+
+
+	return buf;
+}
+
+
+
 /*Задача №3
 
    Написать программу, выводящую на экран треугольник из звёздочек
